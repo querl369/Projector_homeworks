9dba9dfc420fa037cc514528ed15bf288dfce200
querl369 <67265440+querl369@users.noreply.github.com>
2023-04-10 01:56:59 +0200

    excersises reworked

--- arrays.js
+++ arrays.js
@@ -4,22 +4,34 @@
 
 const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
 let initials = [];
-let initialsShorten = [];
 
 if (userNames.length > 0 && userNames !== undefined) {
 	for (let i = 0; i < userNames.length; i++) {
 		let strInitial = '';
 		let element = userNames[i].split(' ');
-			for (var n = 0; n < element.length; n++) {
+		for (var n = 0; n < element.length; n++) {
 			strInitial += element[n][0] + '.';
 		}
-	initialsShorten.push(strInitial);
+
+	initials.push(strInitial);
+
 	}
-	initials = [...initialsShorten.sort()];
+
+	initials.sort();
 }
 
-console.log(initialsShorten);
 console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
+
+//OR
+
+const userNames2 = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
+
+
+const initials1 = userNames2.sort().map(name => {
+    return name.split(" ").map(part => part[0]).join('.') + '.';
+})
+
+console.log(initials1); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 
 /* 
 	Задача на розворот числа:
@@ -28,7 +40,26 @@ console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 const currentMaxValue = 4589;
 let reverseMaxValue;
 
-reverseMaxValue = parseInt((currentMaxValue.toString()).split("").reverse().join(""));
+function isInt(n){
+    return Number(n) === n && n % 1 === 0;
+}
+
+function isFloat(n){
+    return Number(n) === n && n % 1 !== 0;
+}
+
+function reverseNumber(value) {
+    let reverseMaxValue = (value.toString()).split("").reverse().join("");
+
+    if (isInt(value)) {
+    	reverseMaxValue = (value > 0) ? parseInt(reverseMaxValue) : parseInt(`-${reverseMaxValue}`);
+    } 
+    if (isFloat(value)){    
+    	reverseMaxValue = (value > 0) ? parseFloat(reverseMaxValue) : parseFloat(`-${reverseMaxValue}`);
+    }
+
+    return reverseMaxValue;
+}
 
 console.log(reverseMaxValue); // 9854
 console.log(typeof reverseMaxValue); // 'number'
