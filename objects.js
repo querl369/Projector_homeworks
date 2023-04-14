674a8dde3ebf8b36b165415168a26a12e46fc6e5
querl369 <67265440+querl369@users.noreply.github.com>
2023-04-14 02:29:14 +0200

    second and third exercises added

--- objects.js
+++ objects.js
@@ -33,5 +33,84 @@ function durationBetweenDates(startDate = new Date(), endDate = new Date(), dime
       break;
   }
 
-  return `${result} ${dimension}`;
+  return `${result} ${dimension}`; // decided to add year, period in years could be calculated (without a leap year)
+}
+
+/*
+	Задача про перетворення об'єкту
+	Допустимо у вас є об'єкт, у якому кожен ключ - це назва товару (одинм словом), а значення - його ціна.
+	Напишіть функцію яка буде всі ключі переводити у нижній регістр, а всі ціни буде заокруглювати до двох знаків після коми.
+
+	Приклад:
+	// приклад об'єкту
+	 const priceData = {
+	Apples: '23.4',
+	BANANAS: '48',
+	oRAngGEs: '48.7584',
+	 };
+	function optimizer(data) {
+	// тут ваш код
+	};
+	let updatedPriceData = optimizer(priceData);
+	console.log(updatedPriceData)    // {apples: '23.40', bananas: '48.00', oranges: '48.76'}
+*/
+
+function updatedPriceData(priceData) {
+	const newObj = {};
+	for (let key in priceData) {
+		newObj[key.charAt(0).toUpperCase() + key.slice(1).toLowerCase()] = parseFloat(priceData[`${key}`]).toFixed(2);
+	}
+	return newObj; // decided to make key name more prettier by capitalize first letter
+}
+
+/* 
+	Задача про рекурсію та ітерацію
+	Напишіть: - функцію яка рекурсивно буде знаходити суму всіх непарних додатніх чисел до якогось числа.
+	Приклад:function recursiveOddSumTo(number) {
+	// тут ваш код
+	};
+	console.log(recursiveOddSumTo(1)) // 1
+	console.log(recursiveOddSumTo(10)) // 25
+	- функцію яка ітеративно (в циклі) буде знаходити суму всіх непарних додатніх чисел до якогось числа.
+	function iterativeOddSumTo(number) {
+
+	// тут ваш код
+
+	};
+
+	console.log(iterativeOddSumTo(1)) // 1
+	console.log(iterativeOddSumTo(10)) // 25
+*/
+
+function iterativeOddSumTo(iterateNumber) {
+	let evenNumbersCounter = 0;
+	for (let i = 0; i <= iterateNumber; i++) {
+		if (i % 2 !== 0) {
+			evenNumbersCounter += i;
+		}
+	}
+	return evenNumbersCounter;
+}
+
+// OR
+
+function iterativeOddSumTo(iterateNumber) {
+	let evenNumbersCounter = 0;
+	for (let i = 1; i <= iterateNumber; i += 2) {
+		evenNumbersCounter += i;
+	}
+	return evenNumbersCounter;
+}
+
+// Recursion
+
+function recursiveOddSumTo(iterateNumber) {
+  if (iterateNumber < 1) {
+    return 0;
+  }
+  if (iterateNumber % 2 !== 0) {
+    return iterateNumber + recursiveOddSumTo(iterateNumber - 2);
+  } else {
+    return recursiveOddSumTo(iterateNumber - 1);
+  }
 }
