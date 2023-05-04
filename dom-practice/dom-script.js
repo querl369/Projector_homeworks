fa945682e8635f5dd1a62b9b9acd7d6930c18416
querl369 <67265440+querl369@users.noreply.github.com>
2023-05-04 14:35:33 +0200

    listeners added, localeStorage defined

--- dom-practice/dom-script.js
+++ dom-practice/dom-script.js
@@ -5,35 +5,60 @@
 // add message under button 'Last turn off: `{DD-MM-YYYY HH:MM:SS}`'
 // change background color to dark-grey when clicked
 // change background color to grey when clicked again
-// button state should be saved in localStorage and stay avter page reload
+// button state should be saved in localStorage and stay after page reload
+
+const heading = document.getElementsByTagName('h1')[0];
+const sign = document.getElementById('sign');
+const student = document.getElementsByClassName('student');
+
+const frame = document.querySelector('.frame');
 
 const button = document.querySelector('.custom-btn');
 const message = document.createElement('p');
-// message.style.marginTop = '10px';
-button.parentElement.appendChild(message);
+
+frame.insertBefore(message, sign);
 
 const buttonStates = {
     on: 'Turn on',
     off: 'Turn off',
 }
 
-console.log(button.textContent);
+document.addEventListener('DOMContentLoaded', () => {
+    const savedState = localStorage.getItem('buttonState');
+    const savedDate = localStorage.getItem('date');
+
+    if (savedState === null) {
+        button.textContent = buttonStates.off;
+        document.body.style.backgroundColor = '#e0e5ec';
+    } else {
+        if (savedState === buttonStates.on && savedDate !== null) {
+            document.body.style.backgroundColor = '#1b1b1b';
+            button.textContent = buttonStates.on;
+            message.textContent = savedDate;
+        } else {
+            document.body.style.backgroundColor = '#e0e5ec';
+            button.textContent = buttonStates.off;
+            message.textContent = savedDate;
+        }
+    }
+});
+
 button.addEventListener('click', () => {
     if (button.textContent === buttonStates.off) {
         button.textContent = buttonStates.on;
         document.body.style.backgroundColor = '#1b1b1b';
         message.textContent = `Last turn off: ${new Date().toLocaleString()}`;
-        // localStorage.setItem('buttonState', buttonStates.on);
-        // localStorage.setItem('message', message.textContent);
-        console.log(button.textContent);
+        localStorage.setItem('buttonState', buttonStates.on);
+        localStorage.setItem('date', message.textContent);
         return;
-    } 
+    }
     if (button.textContent === buttonStates.on) {
         button.textContent = buttonStates.off;
         document.body.style.backgroundColor = '#e0e5ec';
         message.textContent = `Last turn on: ${new Date().toLocaleString()}`;
-        // localStorage.setItem('buttonState', buttonStates.off);
-        // localStorage.setItem('message', message.textContent);
-        console.log(button.textContent);
+        localStorage.setItem('buttonState', buttonStates.off);
+        localStorage.setItem('date', message.textContent);
     }
 });
+
+
